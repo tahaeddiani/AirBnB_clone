1e6c9d853052ca94c725c676484f479fbe6b5cb4
tahaeddiani <tahaeddiani2@gmail.com>
2023-08-10 11:22:48 -0700

    city

---
+++ models/city.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Defines the city model
+"""
+from .base_model import BaseModel
+
+
+class City(BaseModel):
+    """
+    City objects
+    """
+    state_id = ""
+    name = ""
